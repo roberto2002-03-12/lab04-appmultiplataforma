71ff557d5cf8af03944fc0eb67eb8a069b1ef7a7
roberto2002-03-12 <67761057+roberto2002-03-12@users.noreply.github.com>
2022-04-14 20:27:13 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio/Ejercicio/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Diagnostics;
+
+namespace Ejercicio
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Page1());
+            };
+
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new TabbedPage1());
+            };
+
+            Item3.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new CarouselPage1());
+            };
+
+            Item5.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushModalAsync(new ModalPage1());
+            };
+
+            Item6.Clicked += async (sender, e) => 
+            {
+                var answer = await DisplayAlert("Question?",
+                    "Would you like to play a game", "Yes", "No");
+                Debug.WriteLine("Answer: " + answer);
+            };
+        }
+    }
+}
